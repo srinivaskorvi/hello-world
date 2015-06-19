162d17b0015ded860be01356f9e4045d052f1aa6
Srinivas Korvi <srinivaskorvi@yahoo.com>
2015-06-19 12:31:34 +0530

    added test package

---
+++ hello-world/src/test/java/com/logistics/hello_world/test/MyResourceTest.java
@@ -0,0 +1,18 @@
+package com.logistics.hello_world.test;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.logistics.hello_world.MyResource;
+
+
+public class MyResourceTest {
+
+	@Test
+	public void test() {
+	
+		MyResource res = new MyResource();
+		assertEquals("Got it ok!", res.getIt());
+		
+	}
+}
