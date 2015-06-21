c470b29eb41ed50431b2e7a962e0535cf956623d
Srinivas Korvi <srinivaskorvi@yahoo.com>
2015-06-21 12:51:22 +0530

    add unused private variable to fail the code check

--- hello-world/src/main/java/com/logistics/hello_world/MyResource.java
+++ hello-world/src/main/java/com/logistics/hello_world/MyResource.java
@@ -18,7 +18,10 @@ public class MyResource {
      *
      * @return String that will be returned as a text/plain response.
      */
-    @GET
+    
+	private String name;
+	
+	@GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
         return "Got it ok!";
